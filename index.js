9ef2b7a184f930844ff15b0a5fb83ca1fec4f798
Jolly-mz <jollyxllx676@gmail.com>
2024-10-09 12:37:20 +0100

    Made a small change

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//MyPage
+
 var http = require("http"); 
 const port = 8000; 
 
@@ -6,4 +8,4 @@ http.createServer(function(req, res) {
     res.end("Hello World!");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
-}); 
+});
