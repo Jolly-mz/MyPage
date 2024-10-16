374592233b599da811c4527bd303757e241adc34
Jolly-mz <jollyxllx676@gmail.com>
2024-10-16 11:42:47 +0100

    small changes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 8000;
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+    res.end("This is Jolly's page!");
+    res.writeHead(150, { "Content-Type": "text/plain" });
+    res.end("Welcome to my page... blah blah blah");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 });
