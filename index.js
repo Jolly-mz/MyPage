185c3f0bb4bfba8516a50e9dd3cd55d6bd381c89
Jolly-mz <jollyxllx676@gmail.com>
2024-10-16 11:59:28 +0100

    small changes

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    //res.writeHead(200, { "Content-Type": "text/plain" });
     //res.end("This is Jolly's page!");
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(`
         <html>
             <head>
